1516cc729c2c4782ea35b1bacdc96b5593c1ad44
STShark00 <msbow00@gmail.com>
2023-01-23 12:59:28 -0600

    Basic Search Feature: airport as start / end point

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import Ride from "../components/rides/Ride";
 import { useDbUpdate } from "../utils/firebase";
 import { useDbData } from "../utils/firebase";
+import Form from "react-bootstrap/Form";
 
 const Home = () => {
   const [rides] = useDbData("/rides");
 
   const [updateData] = useDbUpdate("/");
+
+  const [searchStart, setSearchStart] = useState('');
+  const [searchEnd, setSearchEnd] = useState('');
 
   let rideArray = [];
 
@@ -32,12 +36,46 @@ const Home = () => {
     );
   }
 
+  const handleToAirport = () => {
+		setSearchEnd('10000 W Balmoral Ave')
+    setSearchStart('')
+	}
+
+  const handleFromAirport = () => {
+		setSearchStart('10000 W Balmoral Ave')
+    setSearchEnd('')
+	}
+
+
+  const filteredRides = rideArray.filter(
+		ride => (!searchStart || (ride[1].start.address === searchStart)) &&
+            (!searchEnd || (ride[1].end.address === searchEnd))
+		)
+
   return (
     <div>
+      <Form.Text className="text-muted me-4">
+          What ride are you looking for?
+      </Form.Text>
+      <Form.Check
+        inline
+        label="To Airport"
+        name="group1"
+        type="radio"
+        onClick={() => handleToAirport()}
+      />
+      <Form.Check
+        inline
+        label="From Airport"
+        name="group1"
+        type="radio"
+        onClick={() => handleFromAirport()}
+      />
+      <hr className="mt-3 mb-3" />
       {typeof rides != "undefined" &&
         rides != null &&
-        rideArray &&
-        rideArray.map((ride, idx) => (
+        filteredRides &&
+        filteredRides.map((ride, idx) => (
           <div key={idx} className="mb-5">
             <Ride ride={ride[1]} id={ride[0]} />
           </div>
