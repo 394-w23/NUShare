ec9db9b7e5b5ad8c499ce3d1b26cb0b81f72929a
Celinadhh <celinaszhao@gmail.com>
2023-01-24 19:35:19 -0600

    fix user bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,11 +36,13 @@ const Home = () => {
     }
 
     rideArray.sort(function(a,b) {
-      if (a[1].passengers.includes(user.uid) && !b[1].passengers.includes(user.uid)){
-        return -1
-      } 
-      if (!a[1].passengers.includes(user.uid) && b[1].passengers.includes(user.uid)){
-        return 1
+      if (user){
+        if (a[1].passengers.includes(user.uid) && !b[1].passengers.includes(user.uid)){
+          return -1
+        } 
+        if (!a[1].passengers.includes(user.uid) && b[1].passengers.includes(user.uid)){
+          return 1
+        }
       }
       a[1].date + " " + a[1].time < b[1].date + " " + b[1].time ? -1 : 1
     });
