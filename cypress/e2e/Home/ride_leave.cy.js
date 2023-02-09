8809f1b115e74d596c93cb14f837a8e5c310c0d1
Derex Wangmang <66037338+derexwangmang@users.noreply.github.com>
2023-02-09 01:02:52 -0600

    Leave test updated to leave intended ride group, added second leave test

--- cypress/e2e/Home/ride_leave.cy.js
+++ cypress/e2e/Home/ride_leave.cy.js
@@ -1,20 +1,39 @@
 describe("Leaving ride tests", () => {
-  it("on click of leave button, user disappears from passenger ;list", () => {
+  it("on click of leave button, user disappears from passenger list", () => {
     cy.visit("/");
 
     // Verifies user in passenger list
-    cy.get("[data-cy=aLEXbeae-5b85-48c3-956b-6bd23eabcd33]")
+    cy.get("[data-cy=5ba1beae-5b85-48c3-956b-6bd23eabcd33]")
       .find("[data-cy=mWWwXj6w8NTQe9gjXxMCkwRSrve5]")
       .should("exist");
 
     // Leaves passenger
-    cy.get("[data-cy=aLEXbeae-5b85-48c3-956b-6bd23eabcd33]")
+    cy.get("[data-cy=5ba1beae-5b85-48c3-956b-6bd23eabcd33]")
       .find("[data-cy=ride-leave]")
       .click();
 
     // List no longer contains passenger
-    cy.get("[data-cy=aLEXbeae-5b85-48c3-956b-6bd23eabcd33]")
+    cy.get("[data-cy=5ba1beae-5b85-48c3-956b-6bd23eabcd33]")
       .find("[data-cy=mWWwXj6w8NTQe9gjXxMCkwRSrve5]")
       .should("not.exist");
+  });
+
+  it("on click of leave button, if user is only passenger, ride disappears", () => {
+    cy.visit("/");
+
+    // Verifies ride with only user exists
+    cy.get("[data-cy=200ee784-2dbd-2610-ba58-4c46f5d09ea4]")
+      .find("[data-cy=mWWwXj6w8NTQe9gjXxMCkwRSrve5]")
+      .should("exist");
+
+    // Leaves ride
+    cy.get("[data-cy=200ee784-2dbd-2610-ba58-4c46f5d09ea4]")
+      .find("[data-cy=ride-leave]")
+      .click();
+
+    // Ride no longer exists
+    cy.get("[data-cy=200ee784-2dbd-2610-ba58-4c46f5d09ea4]").should(
+      "not.exist"
+    );
   });
 });
