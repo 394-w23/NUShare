55b3a0478eadef1154a6e63d28426d5e3250bfa6
Camilo <camilochafloque@gmail.com>
2023-02-27 16:41:14 -0600

    Added initial test that passed

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,10 @@
-import {describe, expect, test} from 'vitest';
-import {fireEvent, render, screen} from '@testing-library/react';
-import App from './App';
+import { describe, expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('counter tests', () => {
-    
-  test("Counter should be 0 at the start", () => {
+describe("unit tests", () => {
+  test("Welcome label for user should appear in the login page", () => {
     render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+    expect(screen.getByText("Welcome to NUShare")).toBeDefined();
   });
-
-  test("Counter should increment by one when clicked", async () => {
-    render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
-  });
-
 });
