37826c422079b081db971a45184a73871c812ace
Derex Wangmang <66037338+derexwangmang@users.noreply.github.com>
2023-02-28 22:32:29 -0600

    Added unit testing with mocked user/ride data

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,49 @@
 import { describe, expect, test } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+import { useProfile } from "./utils/userProfile";
+import { useDbData, useDbUpdate } from "./utils/firebase";
+import * as testData from "../saved-data/database_export/nushare-2b5ce-default-rtdb.json";
+import { when } from "jest-when";
 
-describe("unit tests", () => {
-  test("Welcome label for user should appear in the login page", () => {
+vi.mock("./utils/userProfile");
+vi.mock("./utils/firebase.js");
+
+const testProfile = {
+  displayName: "testUser",
+  email: "testUser@northwestern.edu",
+  profilePic: "https://illustoon.com/photo/590.png",
+};
+
+describe("without authentication", () => {
+  beforeEach(() => {
+    useProfile.mockReturnValue([null]);
     render(<App />);
-    expect(screen.getByText("Welcome to NUShare")).toBeDefined();
+  });
+
+  it("displays sign in page", () => {
+    expect(screen.getByText("SIGN IN")).toBeDefined();
+  });
+});
+
+describe("after authentication", () => {
+  beforeEach(() => {
+    useProfile.mockReturnValue([testProfile]);
+    when(useDbData).calledWith("/rides").mockReturnValue([testData["rides"]]);
+    when(useDbData).calledWith("/users").mockReturnValue([testData["users"]]);
+    useDbUpdate.mockReturnValue([null]);
+    render(<App />);
+  });
+
+  it("verifies user is signed in", () => {
+    expect(screen.getByText("Sign out")).toBeDefined();
+  });
+
+  it("displays ride from mock data", () => {
+    expect(
+      screen.getByText(
+        "Destination: Chicago O'Hare International Airport (ORD)"
+      )
+    ).toBeDefined();
   });
 });
